21e1345d895e3a1c9e2cfd460e420ec614537df7
Sai Vukkem <svukkem@trakglobal.co.uk>
2021-08-11 13:26:21 +0100

    Decimal Comparator

--- src/com/java/masterclass/DecimalComparator.java
+++ src/com/java/masterclass/DecimalComparator.java
@@ -1,7 +1,23 @@
 package com.java.masterclass;
 
 public class DecimalComparator {
-    public static void maareEqualByThreeDecimalPlacesin() {
+    public static boolean areEqualByThreeDecimalPlaces(double num1, double num2) {
+        int param1 = (int) (num1 * 1000);
+        int param2 = (int) (num2 * 1000);
 
+        boolean result = false;
+
+        if (param1 == param2) {
+            result = true;
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(areEqualByThreeDecimalPlaces(-3.1756, -3.175));
+        System.out.println(areEqualByThreeDecimalPlaces(3.175, 3.176));
+        System.out.println(areEqualByThreeDecimalPlaces(3.0, 3.0));
+        System.out.println(areEqualByThreeDecimalPlaces(-3.123, 3.123));
     }
 }
